d41d58f10fa4a28704fb889f379004497d97f660
a10x <arthatixarca@gmail.com>
2023-07-04 01:10:41 +0100

    Create command class

---
+++ src/Command.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+//An abstract class that all commands must inherit
+class Command
+{
+public:
+	virtual std::unique_ptr<std::string> onExecute() = 0;
+};
+
